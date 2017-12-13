87f11388c2b9fcfc72801e910e47f02f6c707410
doanminhdang <minhdang@linux.com>
2017-12-13 15:34:40 +0100

    Start memo of email discussion

---
+++ index.mm
@@ -0,0 +1,122 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#0033ff" CREATED="1513171696461" ID="ID_489313408" MODIFIED="1513174443476" TEXT="B&#xe0;n v&#x1ec1; Renewable energy &#x1edf; VN">
+<edge COLOR="#0033ff" WIDTH="4"/>
+<font NAME="SansSerif" SIZE="20"/>
+<node COLOR="#ff0000" CREATED="1513171736936" ID="ID_1438005829" MODIFIED="1513174492163" POSITION="left" TEXT="C&#xe1;c ti&#xea;u ch&#xed; &#x111;&#x1ecb;nh l&#x1b0;&#x1ee3;ng">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1513171736937" ID="ID_1365661200" MODIFIED="1513174443482" TEXT="LCOE - lifecycle cost c&#x1ee7;a technology">
+<edge COLOR="#990000" WIDTH="1"/>
+<node COLOR="#111111" CREATED="1513171736938" ID="ID_1494341200" MODIFIED="1513174443483" TEXT="&#x111;i&#x1ec7;n gi&#xf3;: kho&#x1ea3;ng 10 UScent/kWh">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1513171736938" ID="ID_1798647351" MODIFIED="1513174443483" TEXT="energy resistance/GDP growth">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1513171736938" ID="ID_1797812763" MODIFIED="1513174443483" TEXT="su&#x1ea5;t &#x111;&#x1ea7;u t&#x1b0; (investment cost per MW installed)">
+<edge COLOR="#990000" WIDTH="1"/>
+<node COLOR="#111111" CREATED="1513171736939" ID="ID_1893615781" MODIFIED="1513174443484" TEXT="&#x110;i&#x1ec7;n gi&#xf3; (&#x1edf; Vi&#x1ec7;t Nam): kho&#x1ea3;ng 1.8 &#x2013; 2 tri&#x1ec7;u USD/MW">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1513171736939" ID="ID_64638770" MODIFIED="1513174443484" TEXT="t&#x1ef7; l&#x1ec7; t&#x103;ng electricity demand / growth rate">
+<edge COLOR="#990000" WIDTH="1"/>
+<node COLOR="#111111" CREATED="1513171736939" ID="ID_103433967" MODIFIED="1513174443484" TEXT="VN: 2.0 (ti&#xea;u th&#x1ee5; &#x111;i&#x1ec7;n t&#x103;ng 12% vs. GDP t&#x103;ng 6%).">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1513171736939" ID="ID_1848123775" MODIFIED="1513174443491" TEXT="energy intensity">
+<edge COLOR="#990000" WIDTH="1"/>
+<node COLOR="#111111" CREATED="1513171736939" ID="ID_500488860" MODIFIED="1513174443517">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <img src="Energy_Intensity_Graph.jpg" />
+  </body>
+</html>
+</richcontent>
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1513171736939" ID="ID_724412773" MODIFIED="1513174443524" TEXT="2014, EI c&#x1ee7;a Vi&#x1ec7;t Nam ch&#x1ec9; v&#xe0;o kho&#x1ea3;ng 137 ktoe/billion USD">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1513171741876" ID="ID_1017456719" MODIFIED="1513174443524" POSITION="left" TEXT="C&#xe1;c th&#xf4;ng tin &#x111;&#x1ecb;nh t&#xed;nh">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1513171741877" ID="ID_183647439" MODIFIED="1513174443524" TEXT="T&#x1eeb; b&#xe0;i c&#x1ee7;a Ph&#x1ea1;m Chi Lan">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1513171720452" ID="ID_751138408" MODIFIED="1513174443525" POSITION="right" TEXT="M&#x1ee5;c ti&#xea;u">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1513171720452" ID="ID_393700890" MODIFIED="1513174443525" TEXT="X&#xe1;c &#x111;&#x1ecb;nh c&#xe1;c thi&#x1ec7;t h&#x1ea1;i do nhi&#x1ec7;t &#x111;i&#x1ec7;n t&#x1eeb; than">
+<edge COLOR="#990000" WIDTH="1"/>
+<node COLOR="#111111" CREATED="1513171720453" ID="ID_77376370" MODIFIED="1513174443526" TEXT="&#xd4; nhi&#x1ec5;m">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1513171720453" ID="ID_1316664313" MODIFIED="1513174443526" TEXT="fossil fuel tr&#x1ea3;i qua chu tr&#xec;nh khai th&#xe1;c - ti&#xea;u th&#x1ee5; r&#x1ea5;t d&#xe0;i,">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1513171720453" ID="ID_521117125" MODIFIED="1513174443527" TEXT="X&#xe1;c &#x111;&#x1ecb;nh c&#xe1;c &#x1b0;u &#x111;i&#x1ec3;m c&#x1ee7;a n&#x103;ng l&#x1b0;&#x1ee3;ng t&#xe1;i t&#x1ea1;o">
+<edge COLOR="#990000" WIDTH="1"/>
+<node COLOR="#111111" CREATED="1513171720454" ID="ID_1587462516" MODIFIED="1513174443527" TEXT="1) &#x111;a d&#x1ea1;ng ngu&#x1ed3;n cung (energy mix)">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1513171720454" ID="ID_721048598" MODIFIED="1513174443528" TEXT="2) do th&#x1ee7;y &#x111;i&#x1ec7;n &#x111;&#xe3; b&#x1ecb; khai th&#xe1;c h&#x1ebf;t ti&#x1ec1;m n&#x103;ng">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1513171720454" ID="ID_1030699905" MODIFIED="1513174443528" TEXT="3) do hi&#x1ec7;n t&#x1ea1;i Vi&#x1ec7;t Nam &#x111;&#xe3; tr&#x1edf; th&#xe0;nh coal net importer">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1513171720454" ID="ID_1999999444" MODIFIED="1513174443528" TEXT="4) do n&#x103;ng l&#x1b0;&#x1ee3;ng t&#xe1;i t&#x1ea1;o gi&#xfa;p ph&#xe2;n b&#x1ed1; ngu&#x1ed3;n cung &#x111;&#x1ed3;ng &#x111;&#x1ec1;u v&#xe0; ph&#xf9; h&#x1ee3;p h&#x1a1;n">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1513171720454" ID="ID_1856376555" MODIFIED="1513174443529" TEXT="5) RE t&#x1ea1;o nhi&#x1ec1;u jobs">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1513171720455" ID="ID_1180291242" MODIFIED="1513174443529" TEXT="6) D&#x1ef1; b&#xe1;o su&#x1ea5;t &#x111;&#x1ea7;u t&#x1b0; cho RE s&#x1ebd; gi&#x1ea3;m d&#x1ea7;n v&#xe0; chi ph&#xed; mua nhi&#xea;n li&#x1ec7;u cho than t&#x103;ng d&#x1ea7;n?">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1513171720455" ID="ID_1401414922" MODIFIED="1513174443529" TEXT="L&#xfd; do n&#xe0;o m&#xe0; nh&#xe0; &#x111;&#x1ea7;u t&#x1b0; n&#xea;n &#x111;&#x1ea7;u t&#x1b0; v&#xe0;o RE &#x1edf; VN?">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1513171720455" ID="ID_52191143" MODIFIED="1513174443529" TEXT="L&#xfd; do n&#xe0;o m&#xe0; nh&#xe0; n&#x1b0;&#x1edb;c VN n&#xea;n &#x1b0;u ti&#xea;n RE?">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+<node COLOR="#ff0000" CREATED="1513171729465" ID="ID_644017121" MODIFIED="1513174486026" POSITION="right" TEXT="C&#xe1;ch ti&#x1ebf;p c&#x1ead;n">
+<edge COLOR="#00b439" WIDTH="2"/>
+<font BOLD="true" NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1513171729465" ID="ID_177440641" MODIFIED="1513174443532" TEXT="Logic">
+<edge COLOR="#990000" WIDTH="1"/>
+<node COLOR="#111111" CREATED="1513171729465" ID="ID_1872489617" MODIFIED="1513174443533" TEXT="gi&#x1ea3;m energy resistance (Nguy&#xea;n)">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" CREATED="1513171729466" ID="ID_1912452613" MODIFIED="1513174443533" TEXT="thuy&#x1ebf;t ph&#x1ee5;c nh&#xe0; n&#x1b0;&#x1edb;c h&#x1ed7; tr&#x1ee3; &#x111;&#x1ec3; cho local ph&#xe1;t tri&#x1ec3;n RE target c&#x1ee7;a h&#x1ecd; (H&#x1eb1;ng)">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" CREATED="1513171729466" ID="ID_591307629" LINK="https://cvdvn.net/2015/05/27/nang-luong-cho-nguoi-ngheo-phan-i/" MODIFIED="1513174443533" TEXT="https://cvdvn.net/2015/05/27/nang-luong-cho-nguoi-ngheo-phan-i/">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1513172180544" ID="ID_1820131869" MODIFIED="1513174443536" TEXT="t&#x1ef7; l&#x1ec7; local content c&#xf2;n qu&#xe1; th&#x1ea5;p trong c&#x1ea3; quy tr&#xec;nh s&#x1ea3;n xu&#x1ea5;t, x&#xe2;y d&#x1ef1;ng, ph&#xe1;t tri&#x1ec3;n v&#xe0; v&#x1ead;n h&#xe0;nh 1 d&#x1ef1; &#xe1;n n&#x103;ng l&#x1b0;&#x1ee3;ng t&#xe1;i t&#x1ea1;o &#x1edf; Vi&#x1ec7;t Nam, ngu&#x1ed3;n nh&#xe2;n l&#x1ef1;c chuy&#xea;n m&#xf4;n (k&#x1ec3; c&#x1ea3; t&#x1b0; v&#x1ea5;n v&#xe0; technicians) c&#xf2;n r&#x1ea5;t thi&#x1ebf;u v&#xe0; y&#x1ebf;u (Chi)">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1513171729466" ID="ID_182892722" MODIFIED="1513174443537" TEXT="&#x110;i&#x1ec1;u ki&#x1ec7;n hi&#x1ec7;n nay">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+<node COLOR="#990000" CREATED="1513171729466" ID="ID_1450933244" MODIFIED="1513174443537" TEXT="D&#x1ef1; b&#xe1;o">
+<edge COLOR="#990000" WIDTH="1"/>
+</node>
+</node>
+</node>
+</map>
